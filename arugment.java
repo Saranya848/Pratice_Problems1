d70323fa36316e024076d73b05b477189f9cbac4
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-01-30 19:49:19 +0530

    Program of command-line arguments

---
+++ arugment.java
@@ -0,0 +1,12 @@
+package command;
+
+public class arugment {
+
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+
+	}
+
+}
