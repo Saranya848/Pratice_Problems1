145d2deba86de67d675d5183c095f8718a7553c2
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-01-30 19:47:32 +0530

    Program to display default values

---
+++ defaultvalues.java
@@ -0,0 +1,20 @@
+package datatypes;
+
+public class defaultvalues {
+	   static boolean value1;
+	   static double value2;
+	   static float value3;
+	   static int value4;
+	   static long value5;
+	   static String value6;
+	public static void main(String[] args) {
+	      System.out.println("boolean = " + value1);
+	      System.out.println("double = " + value2);
+	      System.out.println("float = " + value3);
+	      System.out.println("int = " + value4);
+	      System.out.println("long = " + value5);
+	      System.out.println("String = " + value6);
+
+	}
+
+}
