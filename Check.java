5c6a0385163ab607aa3f5723fc2e63f5854ad812
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-01-30 19:55:40 +0530

    Program to check given two string are equal or not

---
+++ Check.java
@@ -0,0 +1,17 @@
+package stringchecking;
+
+public class Check {
+
+	public static void main(String[] args) {
+        String a = "Bridgelabz";
+        String b = "Bridgelabz";
+
+        if(a.equals(b)){
+            System.out.println("Both strings are equal.");
+        } else {
+            System.out.println("Both strings are not equal.");
+        }
+
+	}
+
+}
